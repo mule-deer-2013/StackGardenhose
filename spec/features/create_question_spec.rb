1b05511d3a043129f431c5443e55d557feb57e7e
cokeefe <chaemobile@gmail.com>
2013-09-19 16:39:12 -0700

    spec support for login, answer spec outline

--- spec/features/create_question_spec.rb
+++ spec/features/create_question_spec.rb
@@ -1,13 +1,28 @@
 require 'spec_helper'
 
 feature 'create answer' do
+  before(:each) do
+
+  end
+
+  let!(:question) { Question.create(
+                            title: "Gouache to oil?",
+                            body: "What is the gouache Vermillion equivallent in oil?",
+                            user_id: 1) }
   let(:answer) { Answer.create(
                             user_id: 1,
-                            question_id: 1,
-                            body: "The solution to the problem is...") }
+                            question_id: question.id,
+                            body: "Gouache...") }
 
   context "when user is logged in" do
-    it "should have form available" do
+    # it "should have form available" do     #For questions
+    #   visit '/'
+    #   page.should have_content("Ask a question")
+    # end
+
+    it "should have form avilable" do
+      visit "/questions/#{question.id}"
+      page.should have_content("Submit an answer")
     end
   end
 
