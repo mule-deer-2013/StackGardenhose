1e2173c340da27b912f7df12e1a9a56b3d845ef8
= <=>
2013-09-20 12:12:22 -0700

    implement Vote model, with polymorphic association to Question and Answer

--- app/models/question.rb
+++ app/models/question.rb
@@ -3,6 +3,9 @@ class Question < ActiveRecord::Base
   belongs_to :user
   has_many :answers
 
+  has_many :votes, :as => :votable
+
+
   validates :title, presence: true
   validates :body, presence: true
   validates :user_id, presence: true
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20130918174855) do
+ActiveRecord::Schema.define(:version => 20130920185909) do
 
   create_table "answers", :force => true do |t|
     t.integer  "user_id"
@@ -35,6 +35,15 @@ ActiveRecord::Schema.define(:version => 20130918174855) do
     t.string   "password_digest"
     t.datetime "created_at",      :null => false
     t.datetime "updated_at",      :null => false
+  end
+
+  create_table "votes", :force => true do |t|
+    t.integer  "value"
+    t.integer  "votable_id"
+    t.string   "votable_type"
+    t.datetime "created_at",   :null => false
+    t.datetime "updated_at",   :null => false
+    t.integer  "user_id"
   end
 
 end
