cad31d25906039ffb8ffcc44d46ea58d020260dd
= <=>
2013-09-20 19:11:59 -0700

    clean up partial locals

--- app/models/answer.rb
+++ app/models/answer.rb
@@ -4,8 +4,6 @@ class Answer < ActiveRecord::Base
   belongs_to :question
 
   has_many :votes, :as => :votable
-
-
   validates_presence_of :body, :user_id, :question_id
-  validates_uniqueness_of :body
+
 end
